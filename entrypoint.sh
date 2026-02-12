7591286a5fb2f3270163b4fb6abbc9d7a051b595
Gyanendra Prakash <gyaanendrap@gmail.com>
2026-02-13 00:38:32 +0530

    some changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py loaddata extrasettings.json
 gunicorn redcrypt.wsgi --bind 0.0.0.0:7000
