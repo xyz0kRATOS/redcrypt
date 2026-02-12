6cc876662d1525d332d8c2466d5b566c1f6c5f50
Gyanendra Prakash <gyaanendrap@gmail.com>
2026-02-13 01:23:39 +0530

    removed debug check switch for db

--- redcrypt/settings.py
+++ redcrypt/settings.py
@@ -121,15 +121,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'redcrypt.wsgi.application'
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.getenv('DB_NAME'),
+#             'USER': os.getenv('DB_USER'),
+#             'PASSWORD': os.getenv('DB_PASSWORD'),
+#             'HOST': os.getenv('DB_HOST'),
+#             'PORT': os.getenv('DB_PORT'),
+#         }
+#     }
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('DB_NAME'),
@@ -139,7 +150,6 @@ else:
             'PORT': os.getenv('DB_PORT'),
         }
     }
-
 
 # sentry_sdk.init(
 #     dsn=os.getenv('SENTRY_DSN'),
