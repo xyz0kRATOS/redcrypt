822ebbe18f404daf8ccf9718a1c4aed0e45ca024
Gyanendra Prakash <gyaanendrap@gmail.com>
2026-02-13 02:15:29 +0530

    comented extra settings form enterypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py loaddata extrasettings.json
+# python manage.py loaddata extrasettings.json do it for only frist deployment
 gunicorn redcrypt.wsgi --bind 0.0.0.0:7000
